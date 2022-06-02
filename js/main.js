24290047ae80bf3b246c3030ce52ca38415c2bf5
Denis Beliakov <65034334+pobelkin@users.noreply.github.com>
2022-06-02 09:59:09 +0200

    Обновлен ESLint до 8.16.0 и записаны функции из ДЗ 1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
+// Функция генерации случайного числа из заданного диапазона
 
+// eslint-disable-next-line no-unused-vars
+const getRandomInt = (min, max) => {
+  if (min < 0 || max < 0) {
+    return -1;
+  }
+
+  if (max < min) {
+    [min, max] = [max, min]
+  }
+
+  let result = Math.random() * (max - min + 1) + min;
+  return Math.floor(result);
+}
+
+//Функция генерации случайного числа с методом .toFixed(), возврат числа с плавающей запятой
+
+// eslint-disable-next-line no-unused-vars
+const getRandomFloat = (min, max, decimal) => {
+  if (min < 0 || max < 0) {
+    return -1;
+  }
+
+  if (max < min) {
+    [min, max] = [max, min]
+  }
+
+  let result = Math.random() * (max - min + 1) + min;
+  return result.toFixed(decimal);
+}
